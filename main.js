8b6e4c5f3202c8d54c096f91fd3e9bca73ce39a6
gurs1kh <gurs1kh@live.com>
2016-01-30 14:06:23 -0800

    re-init

--- main.js
+++ main.js
@@ -69,8 +69,48 @@ Background.prototype.draw = function (ctx) {
 //    Entity.prototype.draw.call(this);
 }
 
-function Goblin(game) {
+function BoundingBox(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
   
+  this.left = x;
+  this.top = y;
+  this.right = this.left + width;
+  this.bottom = this.top + height;
+}
+
+BoundingBox.prototype.collide = function (other) {
+  if (this.right > other.left && this.left < other.right && this.top < other.bottom && this.bottom > other.top) {
+    return true;
+  }
+  return false;
+  
+}
+
+function ViewingCircle(x, y, radius) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+}
+
+ViewingCircle.prototype.collide = function (other) {
+  //Check the distance from the center of the circle to the enemies four corners
+  var distanceC1 = Math.sqrt(((other.left - this.x) * (other.left - this.x)) + ((other.top - this.y) * (other.top - this.y)));
+  var distanceC2 = Math.sqrt(((other.right - this.x) * (other.right - this.x)) + ((other.top - this.y) * (other.top - this.y)));
+  var distanceC3 = Math.sqrt(((other.left - this.x) * (other.left - this.x)) + ((other.bottom - this.y) * (other.bottom - this.y)));
+  var distanceC4 = Math.sqrt(((other.right - this.x) * (other.right - this.x)) + ((other.bottom - this.y) * (other.bottom - this.y)));
+  if (distanceC1 < this.radius || distanceC2 < this.radius || distanceC3 < this.radius || distanceC4 < this.radius) {
+    return true;
+  }
+  return false;
+}
+
+
+
+
+function Goblin(game) {
     this.animation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet2.png"), 94, 128, 33.3, 32, 0.02, 1, true, false);
     
     this.forwardAnimation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet2.png"), 94, 128, 33.3, 32, 0.2, 3, true, false);
@@ -83,17 +123,148 @@ function Goblin(game) {
     this.wleft = false;
     this.wright = false;
     
+    this.seesHero = false;
+    this.atStarting = true;
+    this.walkTowardX = 0;
+    this.walkTowardY = 0;
+    
     this.radius = 100;
     this.ground = 400;
-    this.x = 400;
-    this.y = 400;
-    Entity.call(this, game, this.x, this.y);
+    this.x = 200;
+    this.y = 100;
+    
+    this.startingX = this.x;
+    this.startingY = this.y;
+    
+    this.boxes = true;
+    this.boundingBox = new BoundingBox(this.x + 5, this.y, this.animation.frameWidth + 8, this.animation.frameHeight + 15);
+    
+    Entity.call(this, game, this.x, this.y); 
 }
 
 Goblin.prototype = new Entity();
 Goblin.prototype.constructor = Goblin;
 
 Goblin.prototype.update = function () {
+  this.wforward = false;
+  this.wbackward = false;
+  this.wleft = false;
+  this.wright = false;
+  //if the goblin is within the viewing circle
+  if (this.seesHero || !this.atStarting) {
+    //if the goblin and destination are on the same y axis
+    if (this.walkTowardY === this.y) {
+      //if the destination is to the left of the goblin
+      if (this.walkTowardX < this.x) {
+        this.wleft = true;
+        this.x = this.x - 1;
+        //if the destination is to the right of the goblin
+      } else { 
+        this.wright = true;
+        this.x = this.x + 1;
+      }
+      //if the destination is below the goblin
+    } else if (this.walkTowardY > this.y) {
+      this.wforward = true;
+      //if the destination and goblin are on the same x axis
+      if (this.walkTowardX === this.x) {
+        this.y = this.y + 1;
+        //if the destination is to the left of the goblin
+      } else if (this.walkTowardX < this.x) {
+        this.y = this.y + .75;
+        this.x = this.x - .75;
+        //if the destination is to the right of the goblin
+      } else {
+        this.y = this.y + .75;
+        this.x = this.x + .75;
+      }
+      //if the destination is above the goblin
+    } else {
+      this.wbackward = true;
+      //if the destination and goblin are on the same x axis
+      if (this.walkTowardX === this.x) {
+        this.y = this.y - 1;
+        //if the destination is to the left of the goblin
+      } else if (this.walkTowardX < this.x) {
+        this.y = this.y - .75;
+        this.x = this.x - .75;
+        //if the destination is to the right of the goblin
+      } else {
+        this.y = this.y - .75;
+        this.x = this.x + .75;
+      }
+    }
+  } 
+  
+  if ((this.startingX < this.x + 1 && this.startingX > this.x - 1) && (this.startingY < this.y + 1 && this.startingY > this.y - 1)) {
+    this.atStarting = true;
+    this.wforward = false;
+    this.wbackward = false;
+    this.wleft = false;
+    this.wright = false;
+  } 
+  
+  this.boundingBox = new BoundingBox(this.x + 5, this.y, this.animation.frameWidth + 8, this.animation.frameHeight + 15);
+  Entity.prototype.update.call(this);
+}
+
+Goblin.prototype.draw = function (ctx) {
+  
+    if (this.wforward) {
+      this.forwardAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
+    }
+    else if (this.wbackward) {
+      this.backwardAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
+    }
+    else if (this.wleft) {
+      this.leftAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
+    }    
+    else if (this.wright) {
+      this.rightAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
+    }
+    else {
+      this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
+    }
+    if (this.boxes) {
+      ctx.strokeStyle = "red";
+      ctx.strokeRect(this.boundingBox.x, this.boundingBox.y, this.boundingBox.width, this.boundingBox.height);
+    }
+
+    Entity.prototype.draw.call(this);
+}
+
+
+function Hero(game) {
+  
+    this.animation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet5.png"), 94, 128, 33.3, 32, 0.02, 1, true, false);
+    
+    this.forwardAnimation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet5.png"), 94, 128, 33.3, 32, 0.2, 3, true, false);
+    this.backwardAnimation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet5.png"), 94.5, 224, 33.3, 32, 0.2, 3, true, false);
+    this.leftAnimation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet5.png"), 94, 160, 33.3, 32, 0.2, 3, true, false);
+    this.rightAnimation = new Animation(ASSET_MANAGER.getAsset("enemyPractice/public_html/img/sheet5.png"), 94, 192, 33.3, 32, 0.2, 3, true, false);
+    
+    this.wforward = false;
+    this.wbackward = false;
+    this.wleft = false;
+    this.wright = false;
+    
+    this.radius = 100;
+    this.ground = 400;
+    this.x = 400;
+    this.y = 400;
+    
+    this.boxes = true;
+    this.boundingBox = new BoundingBox(this.x + 5, this.y, this.animation.frameWidth + 8, this.animation.frameHeight + 15);
+    
+    this.viewingCircle = new ViewingCircle(this.boundingBox.x + this.boundingBox.width / 2, this.boundingBox.y + this.boundingBox.height / 2, 200);
+    
+    Entity.call(this, game, this.x, this.y);
+}
+
+Hero.prototype = new Entity();
+Hero.prototype.constructor = Hero;
+
+Hero.prototype.update = function () {
   
     if (this.game.a) {
       this.wleft = true;
@@ -119,10 +290,37 @@ Goblin.prototype.update = function () {
     }
     else this.wright = false;
     
+    for (var i = 0; i < this.game.enemies.length; i++) {
+      var enemy = this.game.enemies[i];
+      if(this.boundingBox.collide(enemy.boundingBox)) {
+        this.removeFromWorld = true;
+        enemy.seesHero = false;
+        if (enemy.x !== enemy.startingX && enemy.y !== enemy.startingY) {
+          enemy.walkTowardX = enemy.startingX;
+          enemy.walkTowardY = enemy.startingY;
+          enemy.atStarting = false;
+        }
+      }
+      else if(this.viewingCircle.collide(enemy.boundingBox)) {
+        enemy.seesHero = true;
+        enemy.walkTowardX = this.x;
+        enemy.walkTowardY = this.y;
+      } else {
+        enemy.seesHero = false;
+        if (enemy.x !== enemy.startingX && enemy.y !== enemy.startingY) {
+          enemy.walkTowardX = enemy.startingX;
+          enemy.walkTowardY = enemy.startingY;
+          enemy.atStarting = false;
+        }
+      }
+    }
+    
+    this.boundingBox = new BoundingBox(this.x + 5, this.y, this.animation.frameWidth + 8, this.animation.frameHeight + 15);
+    this.viewingCircle = new ViewingCircle(this.boundingBox.x + this.boundingBox.width / 2, this.boundingBox.y + this.boundingBox.height / 2, 200);
     Entity.prototype.update.call(this);
 }
 
-Goblin.prototype.draw = function (ctx) {
+Hero.prototype.draw = function (ctx) {
   
     if (this.wforward) {
       this.forwardAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
@@ -139,6 +337,14 @@ Goblin.prototype.draw = function (ctx) {
     else {
         this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1.5);
     }
+    if (this.boxes) {
+      ctx.strokeStyle = "red";
+      ctx.strokeRect(this.boundingBox.x, this.boundingBox.y, this.boundingBox.width, this.boundingBox.height);
+      ctx.strokeStyle = "green";
+      ctx.beginPath()
+      ctx.arc(this.viewingCircle.x, this.viewingCircle.y, this.viewingCircle.radius, 0, 2*Math.PI);
+      ctx.stroke();
+    }
     Entity.prototype.draw.call(this);
 }
 
@@ -146,7 +352,8 @@ Goblin.prototype.draw = function (ctx) {
 
 var ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("enemyPractice/public_html/img/sheet2.png");
+ASSET_MANAGER.queueDownload("img/sheet2.png");
+ASSET_MANAGER.queueDownload("img/sheet5.png");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
@@ -154,10 +361,15 @@ ASSET_MANAGER.downloadAll(function () {
     var ctx = canvas.getContext('2d');
 
     var gameEngine = new GameEngine();
+    var enemies = [];
     var bg = new Background(gameEngine);
+    var hero = new Hero(gameEngine);
     var goblin = new Goblin(gameEngine);
+    enemies.push(goblin);
 
+    gameEngine.enemies = enemies;
     gameEngine.addEntity(bg);
+    gameEngine.addEntity(hero);
     gameEngine.addEntity(goblin);
  
     gameEngine.init(ctx);
