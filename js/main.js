54da68689e4c28ec79abac89f3ff8d889b6250b9
gurs1kh <gurs1kh@live.com>
2016-02-24 14:14:46 -0800

    reorganized PlaceEnemies class

--- js/main.js
+++ js/main.js
@@ -49,91 +49,10 @@ window.onload = function () {
     game.addEntity(new Key(game, 3400, 4300, 1));
     game.addEntity(new Key(game, 3500, 4300, 2));
     game.addEntity(new Key(game, 3600, 4300, 3));
-
-    this.gate = new Gate(game, 3170, 4010);
-    game.addEntity(this.gate);
+    game.addEntity(new Gate(game, 3170, 4010));
     
-   PlaceEnemies_Goblin(game, 10);
-   // PlaceEnemies_Tomb(game, 10);
-    // PlaceEnemies_Undead(game, 10);
-     // PlaceEnemies_Basic(game, 10);
-     // PlaceEnemies_Human(game, 10);
-
-    var blackKnight = new BlackKnight(game, 2900, 4300);
-    enemies.push(blackKnight);
-    game.addEntity(blackKnight);
-
-    var blackRat = new BlackRat(game, 2900, 4500);
-    enemies.push(blackRat);
-    game.addEntity(blackRat);
-
-    var blondeZombie = new BlondeZombie(game, 2900, 4700);
-    enemies.push(blondeZombie);
-    game.addEntity(blondeZombie);
-
-    var brownRat = new BrownRat(game, 2900, 4900);
-    enemies.push(brownRat);
-    game.addEntity(brownRat);
-
-    var death = new Death(game, 2900, 5100);
-    enemies.push(death);
-    game.addEntity(death);
-
-    var dog = new Dog(game, 2900, 5300);
-    enemies.push(dog);
-    game.addEntity(dog);
-
-    var eyepatch = new Eyepatch(game, 2900, 5500);
-    enemies.push(eyepatch);
-    game.addEntity(eyepatch);
-
-    var helmetGoblin = new HelmetGoblin(game, 2900, 5700);
-    enemies.push(helmetGoblin);
-    game.addEntity(helmetGoblin);
-
-    var hood = new Hood(game, 2900, 5900);
-    enemies.push(hood);
-    game.addEntity(hood);
-
-
-    var mummy = new Mummy(game, 3500, 4500);
-    enemies.push(mummy);
-    game.addEntity(mummy);
-
-    var scarf = new Scarf(game, 3500, 4700);
-    enemies.push(scarf);
-    game.addEntity(scarf);
-
-    var skeleton = new Skeleton(game, 3500, 4900);
-    enemies.push(skeleton);
-    game.addEntity(skeleton);
-
-    var skeletonKing = new SkeletonKing(game, 3500, 5100);
-    enemies.push(skeletonKing);
-    game.addEntity(skeletonKing);
-
-    var stitches = new Stitches(game, 3500, 5300);
-    enemies.push(stitches);
-    game.addEntity(stitches);
-
-    var suburbanZombie = new SuburbanZombie(game, 3500, 5500);
-    enemies.push(suburbanZombie);
-    game.addEntity(suburbanZombie);
-
-    var suitZombie = new SuitZombie(game, 3500, 5700);
-    enemies.push(suitZombie);
-    game.addEntity(suitZombie);
-
-    var viking = new Viking(game, 3500, 5900);
-    enemies.push(viking);
-    game.addEntity(viking);
-    
-    var wizardGoblin = new WizardGoblin(game, 3500, 5990);
-    enemies.push(wizardGoblin);
-    game.addEntity(wizardGoblin);
-
-
-
+	game.addEntity(new PlaceEnemies(game, 5));
+	
     game.init(ctx);
     game.start();
   });
--- js/stage/placeEnemies.js
+++ js/stage/placeEnemies.js
@@ -1,156 +1,150 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function PlaceEnemies(game, numBasics, numHumans, numTombs, numUndeads, numGoblins, numDarks) {
-  PlaceEnemies_Basic(game, numBasics);
-  PlaceEnemies_Human(game, numHumans);
-  PlaceEnemies_Tomb(game, numTombs);
-  PlaceEnemies_Undead(game, numUndeads);
-  PlaceEnemies_Goblin(game, numGoblins);
-  PlaceEnemies_Dark(game, numDarks);
-
+function PlaceEnemies(game, num) {
+	this.game = game;
+	this.num = num;
+}
+	
+PlaceEnemies.prototype = new Entity();
+PlaceEnemies.prototype.constructor = PlaceEnemies;
+count = 0;
+PlaceEnemies.prototype.update = function() {
+	// this.placeBasics();
+	if (!count)
+	this.placeHuman();
+	count++;
+	// this.placeTomb();
+	// this.placeUndead();
+	// this.placeGoblin();
+	// this.placeDark();
 }
 
-function PlaceEnemies_Basic(game, numBasics) {
-
-  numBasics = numBasics / 4;
-  
-  PlaceEnemies_BasicLocation(game, numBasics, 1950, 4450, 4500, 6050); //Bottom
-  PlaceEnemies_BasicLocation(game, numBasics, 1950, 4450, 350, 2300); //Top
-  PlaceEnemies_BasicLocation(game, numBasics, 350, 2300, 1950, 4450); //Left
-  PlaceEnemies_BasicLocation(game, numBasics, 4100, 6050, 1950, 4450); //Right
-
+PlaceEnemies.prototype.placeBasics = function() {
+	var num = this.num / 4;
+	
+	this.placeBasicsAtlocation(num, 1950, 4450, 4500, 6050); //Bottom
+	this.placeBasicsAtlocation(num, 1950, 4450, 350, 2300); //Top
+	this.placeBasicsAtlocation(num, 350, 2300, 1950, 4450); //Left
+	this.placeBasicsAtlocation(num, 4100, 6050, 1950, 4450); //Right
 }
 
-function PlaceEnemies_BasicLocation(game, numBasics, XMin, XMax, YMin, YMax) {
-  for (var i = 0; i < numBasics; i++) {
-    var location = getLocation(game, XMin, XMax, YMin, YMax);
-    var basic;
-    var which = getRandomNumber(0, 2);
-    if (which === 0)
-      basic = new Dog(game, location.x, location.y);
-    else if (which === 1)
-      basic = new BlackRat(game, location.x, location.y);
-    else if (which === 2)
-      basic = new BrownRat(game, location.x, location.y);
-    game.enemies.push(basic);
-    game.addEntity(basic);
-  }   
-  
+PlaceEnemies.prototype.placeBasicsAtlocation = function(num, xMin, xMax, yMin, yMax) {
+	for (var i = 0; i < num; i++) {
+		var location = getlocation(xMin, xMax, yMin, yMax);
+		var basic;
+		var which = getRandomNumber(0, 2);
+		if (which === 0)
+			basic = new Dog(this.game, location.x, location.y);
+		else if (which === 1)
+			basic = new BlackRat(this.game, location.x, location.y);
+		else if (which === 2)
+			basic = new BrownRat(this.game, location.x, location.y);
+		this.game.enemies.push(basic);
+		this.game.addEntity(basic);
+	}   
 }
 
-function PlaceEnemies_Human(game, numHumans) {
-  var blackKnight = new BlackKnight(game, 500, 500);
-  game.enemies.push(blackKnight);
-  game.addEntity(blackKnight);
-
-  for (var i = 0; i < numHumans; i++) {
-    var location = getLocation(game, 400, 1900, 350, 1910);
-    var human;
-    var which = getRandomNumber(0, 2);
-    if (which === 0)
-      human = new Stitches(game, location.x, location.y);
-    else if (which === 1)
-      human = new Eyepatch(game, location.x, location.y);
-    else if (which === 2)
-      human = new Viking(game, location.x, location.y);
-    game.enemies.push(human);
-    game.addEntity(human);
-  }
-
+PlaceEnemies.prototype.placeHuman = function() {
+	var blackKnight = new BlackKnight(this.game, 500, 500);
+	this.game.enemies.push(blackKnight);
+	this.game.addEntity(blackKnight);
+	
+	for (var i = 0; i < this.num; i++) {
+		var location = getlocation(400, 1900, 350, 1910);
+		var human;
+		var which = getRandomNumber(0, 2);
+		if (which === 0)
+			human = new Stitches(this.game, location.x, location.y);
+		else if (which === 1)
+			human = new Eyepatch(this.game, location.x, location.y);
+		else if (which === 2)
+		human = new Viking(this.game, location.x, location.y);
+		this.game.enemies.push(human);
+		this.game.addEntity(human);
+	}
 }
 
-function PlaceEnemies_Tomb(game, numTombs) {
-  var skeletonKing = new SkeletonKing(game, 500, 5900);
-  game.enemies.push(skeletonKing);
-  game.addEntity(skeletonKing);
+PlaceEnemies.prototype.placeTomb = function() {
+	var skeletonKing = new SkeletonKing(this.game, 500, 5900);
+	this.game.enemies.push(skeletonKing);
+	this.game.addEntity(skeletonKing);
 
-  for (var i = 0; i < numTombs; i++) {
-    var location = getLocation(game, 400, 1900, 4475, 6040);
-    var tomb;
-    var which = getRandomNumber(0, 1);
-    if (!which)
-      tomb = new Skeleton(game, location.x, location.y);
-    else
-      tomb = new Mummy(game, location.x, location.y);
-    game.enemies.push(tomb);
-    game.addEntity(tomb);
-  }
-
+	for (var i = 0; i < this.num; i++) {
+		var location = getlocation(400, 1900, 4475, 6040);
+		var tomb;
+		var which = getRandomNumber(0, 1);
+		if (!which)
+			tomb = new Skeleton(this.game, location.x, location.y);
+		else
+			tomb = new Mummy(this.game, location.x, location.y);
+		this.game.enemies.push(tomb);
+		this.game.addEntity(tomb);
+	}
 }
 
-function PlaceEnemies_Undead(game, numUndeads) {
-  var death = new Death(game, 5820, 500);
-  game.enemies.push(death);
-  game.addEntity(death);
+PlaceEnemies.prototype.placeUndead = function() {
+	var death = new Death(this.game, 5820, 500);
+	this.game.enemies.push(death);
+	this.game.addEntity(death);
 
-  for (var i = 0; i < numUndeads; i++) {
-    var location = getLocation(game, 4475, 6040, 350, 1910);
-    var undead;
-    var which = getRandomNumber(0, 2);
-    if (which === 0)
-      undead = new SuitZombie(game, location.x, location.y);
-    else if (which === 1)
-      undead = new BlondeZombie(game, location.x, location.y);
-    else if (which === 2)
-      undead = new SuburbanZombie(game, location.x, location.y);
-    game.enemies.push(undead);
-    game.addEntity(undead);
-  }
-
+	for (var i = 0; i < this.num; i++) {
+		var location = getlocation(this.game, 4475, 6040, 350, 1910);
+		var undead;
+		var which = getRandomNumber(0, 2);
+		if (which === 0)
+			undead = new SuitZombie(this.game, location.x, location.y);
+		else if (which === 1)
+			undead = new BlondeZombie(this.game, location.x, location.y);
+		else if (which === 2)
+			undead = new SuburbanZombie(this.game, location.x, location.y);
+		this.game.enemies.push(undead);
+		this.game.addEntity(undead);
+	}
 }
 
-function PlaceEnemies_Goblin(game, numGoblins) {
-  var armoredGoblin = new ArmoredGoblin(game, 5820, 5900);
-  game.enemies.push(armoredGoblin);
-  game.addEntity(armoredGoblin);
+PlaceEnemies.prototype.Goblin = function() {
+	var armoredGoblin = new ArmoredGoblin(this.game, 5820, 5900);
+	this.game.enemies.push(armoredGoblin);
+	this.game.addEntity(armoredGoblin);
 
-  for (var i = 0; i < numGoblins; i++) {
-    var location = getLocation(game, 4475, 6040, 4475, 6040);
-    var goblin;
-    var which = getRandomNumber(0, 1);
-    if (!which)
-      goblin = new WizardGoblin(game, location.x, location.y);
-    else
-      goblin = new HelmetGoblin(game, location.x, location.y);
-    game.enemies.push(goblin);
-    game.addEntity(goblin);
-  }
-
+	for (var i = 0; i < this.num; i++) {
+		var location = getlocation(4475, 6040, 4475, 6040);
+		var goblin;
+		var which = getRandomNumber(0, 1);
+		if (!which)
+			goblin = new WizardGoblin(this.game, location.x, location.y);
+		else
+			goblin = new HelmetGoblin(this.game, location.x, location.y);
+		this.game.enemies.push(goblin);
+		this.game.addEntity(goblin);
+	}
 }
 
-function PlaceEnemies_Dark(game, numDarks) {
-
-
+PlaceEnemies.prototype.placeDark = function() {
+	
 }
 
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1.0)) + min;
+	return Math.floor(Math.random() * (max - min + 1.0)) + min;
 }
 
-function getLocation(game, XMin, XMax, YMin, YMax) {
-  var Location = {};
-  var newX = true;
-  var newY = true;
-  while (1) {
-      if (newX)
-        Location.x = getRandomNumber(XMin, XMax);
-      if (newY)
-        Location.y = getRandomNumber(YMin, YMax);
-      newX = false;
-      newY = false;
-      for (var j = 0; j < game.enemies.length; j++) {
-        if (Math.abs(game.enemies[j].x - Location.x) <= 100) 
-          newX = true;
-        if (Math.abs(game.enemies[j].y - Location.y) <= 100) 
-          newY = true;
-      }
-      if (!newX && !newY)
-        break;
-    }
-  
-  return Location;
+function getlocation(xMin, xMax, yMin, yMax) {
+	var location = {};
+	var newX = true;
+	var newY = true;
+	while (true) {
+		if (newX)
+			location.x = getRandomNumber(xMin, xMax);
+		if (newY)
+			location.y = getRandomNumber(yMin, yMax);
+		newX = false;
+		newY = false;
+		for (var j = 0; j < this.game.enemies.length; j++) {
+		if (Math.abs(this.game.enemies[j].x - location.x) <= 100) 
+			newX = true;
+		if (Math.abs(this.game.enemies[j].y - location.y) <= 100) 
+			newY = true;
+		}
+		if (!newX && !newY)
+			break;
+	}
+	return location;
 }
