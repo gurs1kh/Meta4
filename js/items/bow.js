cfce6db24d849e7625b21310032cde1d0bc89dc9
gurs1kh <gurs1kh@live.com>
2016-02-25 13:26:42 -0800

    fixed arrow issue

--- js/items/bow.js
+++ js/items/bow.js
@@ -1,6 +1,6 @@
 function Bow(game, x, y, pickedUp) {
 	Weapon.call(this, game, x, y, pickedUp);
-	this.attackDelay = 2000;
+	this.attackDelay = 3000;
 	this.width = 25;
 	this.height = 25;
 	this.offX = 20;
@@ -15,11 +15,11 @@ Bow.prototype.update = function() {
 	if (this.attackingTime <= 0 && this.pickedUp) {
 		if (this.game.left)
 			this.game.addEntity(new Arrow(this.game, this.x, this.y, this.damage, 0));
-		if (this.game.down)
+		else if (this.game.down)
 			this.game.addEntity(new Arrow(this.game, this.x, this.y, this.damage, 1));
-		if (this.game.right)
+		else if (this.game.right)
 			this.game.addEntity(new Arrow(this.game, this.x, this.y, this.damage, 2));
-		if (this.game.up)
+		else if (this.game.up)
 			this.game.addEntity(new Arrow(this.game, this.x, this.y, this.damage, 3));
 	}
 }
