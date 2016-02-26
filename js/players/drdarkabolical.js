7ff758b268b2fcec5b9b337b7b443b8a727aab29
gurs1kh <gurs1kh@live.com>
2016-02-26 14:37:49 -0800

    updated final boss

--- js/players/drdarkabolical.js
+++ js/players/drdarkabolical.js
@@ -14,7 +14,7 @@ function DrDarkabolical(game, x, y) {
 	this.telOutAnimation = new Animation(sheet, 192 + 96, 128, 32, 32, 0.05, 9, false, true);
     this.telInAnimation = new Animation(sheet, 192 + 96, 128, 32, 32, 0.05, 9, false, false);
     
-	this.telIn = true;
+	this.telIn = false;
 	this.telOut = false;
 }
 
@@ -24,8 +24,39 @@ DrDarkabolical.prototype.constructor = DrDarkabolical;
 DrDarkabolical.prototype.update = function() {
 	Boss.prototype.update.call(this);
 	this.speed = this.game.hero.speed;
+	if (this.canSee(this.game.hero)) {
+		this.radius = 6400;
+	}
+	if (this.telOut) {
+        if (this.telOutAnimation.isDone()) {
+            this.telOutAnimation.elapsedTime = 0;
+            this.telOut = false;
+			this.telIn = true;	
+			this.x += getRandomNumber(-250, 250);
+			this.y += getRandomNumber(-250, 250);
+        }
+    } else if (this.telIn) {
+		if (this.telInAnimation.isDone()) {
+            this.telInAnimation.elapsedTime = 0;
+            this.telIn = false;
+        }
+	}
 }
 
-// DrDarkabolical.prototype.draw = function() {
-	
-// }
+DrDarkabolical.prototype.draw = function (ctx) {
+    if (this.telOut) {
+        this.telOutAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    } else if (this.telIn) {
+		this.telInAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);	
+	} else if (this.up) {
+		this.upAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);	
+	} else if (this.down) {
+		this.downAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);	
+	} else if (this.left) {
+		this.leftAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);	
+	} else if (this.right) {
+		this.rightAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);	
+	} else {
+        this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
+}
