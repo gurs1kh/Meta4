1bf42b4e898e796d2b2061c025460c3bddb83849
Todd Robbins <toddrobb11@gmail.com>
2016-03-09 11:58:48 -0800

    added rocks and pit image

--- js/stage/terrain/rock.js
+++ js/stage/terrain/rock.js
@@ -1,12 +1,11 @@
 function Rock(game, x, y) {
 	Terrain.call(this, game, x, y);
-	this.width = 63;
-	this.height = 77;
+	this.width = 35;
+	this.height = 35;
 	this.sheet = ASSET_MANAGER.getAsset("img/sheetRock.png");
-	var which = getRandomNumber(0, 0);
-	if (which === 0) this.animation = new Animation(this.sheet, 0, 0, 63, 77, 0.2, 1, true, false);
-	else if (which === 1) this.animation = new Animation(this.sheet, 33, 0, 32, 31, 0.2, 1, true, false);
-	else if (which === 2) this.animation = new Animation(this.sheet, 33, 0, 32, 31, 0.2, 1, true, false);
+	var which = getRandomNumber(0, 1);
+	if (which === 0) this.animation = new Animation(this.sheet, 0, 0, 35, 46, 0.2, 1, true, false);
+	else if (which === 1) this.animation = new Animation(this.sheet, 52, 8, 33, 36, 0.2, 1, true, false);
 	this.closed = true;
 }
 Rock.prototype = new Terrain();
